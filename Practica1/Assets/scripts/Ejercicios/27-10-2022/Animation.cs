cccaf4d9f40071f7ee0031b9a03da85155eab267
yeray29 <yeeraay29@gmail.com>
2022-10-27 18:19:43 +0200

    commit second

---
+++ Practica1/Assets/scripts/Ejercicios/27-10-2022/Animation.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animation : MonoBehaviour
+{
+    public Animator myAnimator;
+    void Update()
+    {
+        
+    }
+}
